f4e341069ce145d5617e5af8a9ce5f4d969e72cc
Dave Sommerville <dave.r.sommerville@outlook.com>
2026-01-12 18:04:21 -0600

    Tidying up the engine

--- game_engine/engine.py
+++ game_engine/engine.py
@@ -1,17 +1,10 @@
 from .game_states import GameState
 from .game_action_error import GameActionError
-from .locations.dungeon import Dungeon
-from .entities.characters.character import Character
 from .events.combat import CombatEvent
-from .events.skill_contest import SkillContestEvent
 from .events.merchant_interaction import MerchantEvent
 from .events.inventory_item import InventoryItemEvent
 from .utilities.rng_utilities import weighted_decision
 from .factory import enemy_factory, merchant_factory, trap_factory
-from .entities.contest_object import ContestObject
-from .entities.characters.npc import NPC
-from .entities.items.item import Item
-from .entities.items.armor import Armor
 
 class GameEngine:
     def __init__(self):
@@ -29,11 +22,9 @@ class GameEngine:
                 # Attempt the game logic for the current state
                 self._resolve_state_action(dungeon, action)
         except GameActionError as e:
-            # If an error happens, we catch it here
             self.error = str(e)
         finally:
-            # This block runs NO MATTER WHAT (success or error)
-            self.game_over_check(dungeon)
+            self._game_over_check(dungeon)
             dungeon.message_buffer.extend(self.logs)
             self.logs = dungeon.message_buffer
             dungeon.message_buffer = []
@@ -66,38 +57,26 @@ class GameEngine:
                 self._resolve_spell_management_menu(dungeon, action)
             case _:
                 raise GameActionError("Invalid State")
+            
+    @staticmethod
+    def get_current_menu(dungeon):
+        if dungeon.current_event:
+            return dungeon.current_event.get_options()
+        match dungeon.state:
+            case GameState.MAIN_MENU:
+                actions_list = dungeon.player.current_chamber.move_actions()
+                actions_list.extend(
+                    ["search", "rest", "inventory", "spells", "details", "describe"]
+                )
+                return actions_list
+            case GameState.INVENTORY_MANAGEMENT:
+                return dungeon.player.print_character_inventory()
+            case GameState.SPELL_MANAGEMENT:
+                return ["cast", "inspect", "back"]
+            case _:
+                raise GameActionError("Invalid Action")
 
-    def _call_for_combat_event(self, dungeon):
-        if weighted_decision(0.5):
-            character = enemy_factory()
-            event = CombatEvent(character)
-            self._log(f"You are attacked by an aggressive enemy")
-            self._log(f"{character.name}")
-            self._log(f"{character.description}")
-            self._log("Choose your next action wisely")
-            dungeon.current_event = event
 
-    def _take_damage(self):
-        self._log("You take damage")
-        pass
-    def _take_half_damage(self):
-        self._log("You take half damage")
-
-    def _call_for_contest(self, dungeon):
-        contest_obj = trap_factory()
-        self._log(f"You see a {contest_obj.name} trigger ahead of you")
-        self._log(f"It is {contest_obj.description}")
-        result = contest_obj.trigger_trap(dungeon.player)
-        self._log(result)
-
-    def _call_for_merchant_event(self, dungeon):
-        dungeon.state = GameState.INVENTORY_MANAGEMENT
-        merchant = merchant_factory()
-        merchant_interaction = MerchantEvent(merchant)
-        self._log(f"You meet a merchant named {merchant.name}")
-        self._log("Select an item to purchase if you wish")
-        dungeon.current_event = merchant_interaction
-    # Has to check the room's direction for sneaky players
     def _resolve_main_menu(self, dungeon, action):
         match action:
             case "move north":  # Sub events
@@ -134,14 +113,6 @@ class GameEngine:
                 self._log(chamber.description)
             case _:
                 raise GameActionError("Invalid Action")
-
-    def game_over_check(self, dungeon):
-        if dungeon.player.character_death_check():
-            self._log("You died")
-            dungeon.current_event = None
-            dungeon.state = GameState.GAME_OVER
-            print("You died")
-
     def _resolve_inventory_management_menu(self, dungeon, action):
         player = dungeon.player
         if action == "back":
@@ -169,23 +140,35 @@ class GameEngine:
 
     def _resolve_spell_management_menu(self, dungeon, action):
         pass
+            
+    def _call_for_combat_event(self, dungeon):
+        if weighted_decision(0.5):
+            character = enemy_factory()
+            event = CombatEvent(character)
+            self._log(f"You are attacked by an aggressive enemy")
+            self._log(f"{character.name}")
+            self._log(f"{character.description}")
+            self._log("Choose your next action wisely")
+            dungeon.current_event = event
 
-    @staticmethod
-    # Oddly placed static function *****
-    def get_current_menu(dungeon):
-        if dungeon.current_event:
-            return dungeon.current_event.get_options()
+    def _call_for_contest(self, dungeon):
+        contest_obj = trap_factory()
+        self._log(f"You see a {contest_obj.name} trigger ahead of you")
+        self._log(f"It is {contest_obj.description}")
+        result = contest_obj.trigger_trap(dungeon.player)
+        self._log(result)
 
-        match dungeon.state:
-            case GameState.MAIN_MENU:
-                actions_list = dungeon.player.current_chamber.move_actions()
-                actions_list.extend(
-                    ["search", "rest", "inventory", "spells", "details", "describe"]
-                )
-                return actions_list
-            case GameState.INVENTORY_MANAGEMENT:
-                return dungeon.player.print_character_inventory()
-            case GameState.SPELL_MANAGEMENT:
-                return ["cast", "inspect", "back"]
-            case _:
-                raise GameActionError("Invalid Action")
+    def _call_for_merchant_event(self, dungeon):
+        dungeon.state = GameState.INVENTORY_MANAGEMENT
+        merchant = merchant_factory()
+        merchant_interaction = MerchantEvent(merchant)
+        self._log(f"You meet a merchant named {merchant.name}")
+        self._log("Select an item to purchase if you wish")
+        dungeon.current_event = merchant_interaction
+
+    def _game_over_check(self, dungeon):
+        if dungeon.player.character_death_check():
+            self._log("You died")
+            dungeon.current_event = None
+            dungeon.state = GameState.GAME_OVER
+            print("You died")
